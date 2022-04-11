6fc6009c5aa77857985ce6cb8943ddd03a706495
dweverson <dweverson@gmail.com>
2022-04-11 20:42:12 +0100

    page 0 data fetched and displayed in table

--- react-tech-test/src/Solution/YourSolution.js
+++ react-tech-test/src/Solution/YourSolution.js
@@ -1,11 +1,28 @@
 import '../AdditionalFiles/App.css';
 import * as React from "react";
+import { useEffect, useState } from 'react';
+
 
 //This is the API url to fetch from
 const API_URL = 'https://matchesfashion.com/api/products';
 const TAX_RATE = 0.08;
 
 function YourSolution() {
+
+  const [productData, setProductData] = useState([]);
+
+
+  useEffect(() => {
+    fetch(`${API_URL}?page=0`)
+    .then((response) => response.json())
+    .then((body) => {
+      setProductData(body.products)
+    
+    })
+    .catch(err => console.log(err, 'ERROR'));
+
+  }, []);
+
     return (
     <div className="App">
       <table id="products">
@@ -20,6 +37,16 @@ function YourSolution() {
         </tr>
         </thead>
           <tbody>
+          {productData.map((product) => (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.brand}</td>
+                <td>{product.name}</td>
+                <td>{product.quantitySold}</td>
+                <td>{product.soldPrice}</td>
+                <td>{product.costToBusiness}</td>
+              </tr>
+            ))}
           </tbody>
       </table>
       <button>First Page</button>
